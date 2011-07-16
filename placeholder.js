01e97a883c5615ceed51ace884448031f491a79e
Blake Gardner <blakegardner@cox.net>
2011-07-16 07:23:48 +0000

    Fixed a bug where the field was not being binded on browser refresh. Added some TODO's. Added a local copy of jQuery and Modernizr

--- placeholder.js
+++ placeholder.js
@@ -1,10 +1,11 @@
-
-// http://davidwalsh.name/html5-placeholder
-// http://api.jquery.com/bind/
-
 /**
  * Adds HTML5 placeholder attribute support to browsers
  * that do not support this feature.
+ */
+
+/**
+ * TODO: Add support for unbinding inputs that get changed via JS after initialization
+ * TODO: Add support for <textarea> elements
  */
 
 (function( $ ) {
@@ -21,27 +22,31 @@
 
 
         // loop over all inputs
-        $('input[type="text"][dtext][value=""][dtext!=""]', this).each(function() {
+        $('input[type="text"][dtext]', this).each(function() {
             
-            // set the placeholder values
-            $(this).val($(this).attr('dtext'));
-            $(this).addClass(options.defaultClass);
-            
-            
-            $(this).bind({
-                focus: function() {
-                    if($(this).val() == $(this).attr('dtext')) {
-                        $(this).val("");
-                        $(this).removeClass(options.defaultClass);
+            if($(this).val() == "" || $(this).val() == $(this).attr('dtext')) {
+                
+                // set the placeholder values
+                $(this).val($(this).attr('dtext'));
+                $(this).addClass(options.defaultClass);
+
+
+                $(this).bind({
+                    focus: function() {
+                        if($(this).val() == $(this).attr('dtext')) {
+                            $(this).val("");
+                            $(this).removeClass(options.defaultClass);
+                        }
+                    },
+                    blur: function() {
+                        if($(this).val() == "") {
+                            $(this).addClass(options.defaultClass);
+                            $(this).val($(this).attr('dtext'));
+                        }
                     }
-                },
-                blur: function() {
-                    if($(this).val() == "") {
-                        $(this).addClass(options.defaultClass);
-                        $(this).val($(this).attr('dtext'));
-                    }
-                }
-            }); // bind
+                }); // bind
+                
+            }
             
             
         }); // end each loop
